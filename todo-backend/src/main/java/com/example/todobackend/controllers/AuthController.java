eac4d41ce557f5e8d1680c1acde62c076722e6c4
Daniel <daniellaic4@gmail.com>
2021-03-02 16:48:28 +0100

    added crossorigin to the controller

--- todo-backend/src/main/java/com/example/todobackend/controllers/AuthController.java
+++ todo-backend/src/main/java/com/example/todobackend/controllers/AuthController.java
@@ -4,11 +4,13 @@ import com.example.todobackend.models.User;
 import com.example.todobackend.services.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
