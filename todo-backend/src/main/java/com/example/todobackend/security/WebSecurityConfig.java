9080453f1b3a950d6f8b11aa93f4737ccded99b1
Daniel <daniellaic4@gmail.com>
2021-03-03 12:11:49 +0100

    changed package name

--- todo-backend/src/main/java/com/example/todobackend/security/WebSecurityConfig.java
+++ todo-backend/src/main/java/com/example/todobackend/security/WebSecurityConfig.java
@@ -1,5 +1,4 @@
-package com.example.todobackend.securingweb;
-
+package com.example.todobackend.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
